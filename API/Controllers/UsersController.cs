32dd829a63ea61cc4d3a4bc1949f5eaf9d752608
Elbaum-B <elbaum.b@gmail.com>
2024-08-04 11:28:26 +0300

    Repare Section 2

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,14 +7,9 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 [ApiController]
 [Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+public class UsersController(DataContext context) : ControllerBase
 {
-    private readonly DataContext _context;
 
-    public UsersController(DataContext context)
-    {
-        _context = context;
-    }
 
     // [HttpGet]
     // public ActionResult<IEnumerable<AppUser>> GetUsers(){
@@ -22,10 +17,12 @@ public class UsersController : ControllerBase
     //     return users;
     // }
 
-    [HttpGet("{id}")] // /api/users/2
+    [HttpGet("{id:int}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        var user = await _context.Users.FindAsync(id);
+        var user = await context.Users.FindAsync(id);
+        if(user == null)
+            return NotFound();
         return user;
     }
 
@@ -34,7 +31,7 @@ public class UsersController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await _context.Users.ToListAsync();
+        var users = await context.Users.ToListAsync();
         return users;
     }
 }
